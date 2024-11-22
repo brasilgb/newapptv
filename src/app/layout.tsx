62e77f3143e17d2551697d4b3b32498628d8ec74
Anderson Brasil <andersonbrasil72@gmail.com>
2024-11-22 11:34:19 -0300

    Push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Roboto, Poppins } from 'next/font/google'
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700', '900'],
+  display: 'swap',
+  variable: '--font-roboto'
+})
+
+const poppins = Poppins({
+  weight: ['300', '400', '500', '700', '900'],
+  display: 'swap',
+  variable: '--font-poppins'
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +27,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.className} ${poppins.className} antialiased`}
       >
-        {children}
+        <div>
+
+          <div>
+            {children}
+          </div>
+          
+        </div>
       </body>
     </html>
   );
